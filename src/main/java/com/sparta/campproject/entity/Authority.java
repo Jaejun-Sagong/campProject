a715aef032da587b492ca97ef8fe50a466553538
JaejunSagong <jaejun7407@naver.com>
2022-08-13 15:05:27 +0900

    게시글 기능 구현

--- src/main/java/com/sparta/campproject/entity/Authority.java
+++
@@ -1,6 +0,0 @@
-
-package com.sparta.campproject.entity;
-
-public enum Authority {
-    ROLE_USER, ROLE_ADMIN
-}
--- src/main/java/com/sparta/campproject/repository/CampRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.campproject.repository;
-
-import com.sparta.campproject.entity.Camp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CampRepository extends JpaRepository<Camp, Long> {
-
-}
