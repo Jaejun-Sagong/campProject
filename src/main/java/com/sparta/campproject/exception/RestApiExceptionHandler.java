fbba4279b878c51177f90c59285d2347d56f9dc8
MungT <ele0214@kakao.com>
2022-08-15 08:07:51 +0900

    예외처리 400 bad request 완료

---
+++ src/main/java/com/sparta/campproject/exception/RestApiExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.sparta.campproject.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RestApiExceptionHandler {
+
+    @ExceptionHandler({RuntimeException.class, UsernameNotFoundException.class})
+    public ResponseEntity<Object> handleApiRequestException(Exception ex) {
+        RestApiException restApiException = new RestApiException();
+        restApiException.setHttpStatus(HttpStatus.BAD_REQUEST);
+        restApiException.setErrorMessage(ex.getMessage());
+
+        return new ResponseEntity(
+                restApiException,
+                HttpStatus.BAD_REQUEST
+        );
+    }
+}
